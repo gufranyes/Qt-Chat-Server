2dadb13892f589a3e94842cee2eddedaa9279f55
gufranyes <gufran.yesilyurt@ozu.edu.tr>
2021-11-07 22:29:59 +0300

    tcpServer classes added

---
+++ tcpserver.cpp
@@ -0,0 +1,55 @@
+#include "tcpserver.h"
+
+tcpServer::tcpServer(QObject *parent) : QObject(parent)
+{
+    connect(&_server, &QTcpServer::newConnection, this, &tcpServer::onNewConnection);
+    connect(this, &tcpServer::newMessage, this, &tcpServer::onNewMessage);
+    if(_server.listen(QHostAddress::Any, 45000)){
+        qInfo() << "Listening Succesfully...";
+    }
+}
+
+void tcpServer::onNewConnection()
+{
+    const auto client = _server.nextPendingConnection();
+    if(client == nullptr){return;}
+
+    qInfo() << "New client connected.";
+
+    _clients.insert(this->getClientKey(client), client);
+
+    connect(client, &QTcpSocket::readyRead, this, &tcpServer::onReadyRead);
+    connect(client, &QTcpSocket::disconnected, this, &tcpServer::onDisconnected);
+}
+
+void tcpServer::onReadyRead()
+{
+    const auto client = qobject_cast<QTcpSocket*>(sender());
+    if(client == nullptr){return;}
+
+    const auto messages = this->getClientKey(client).toUtf8() + ": " + client->readAll();
+
+    emit newMessage(messages);
+}
+
+void tcpServer::onDisconnected()
+{
+    const auto client = qobject_cast<QTcpSocket*>(sender());
+    if(client == nullptr){return;}
+
+    _clients.remove(this->getClientKey(client));
+}
+
+void tcpServer::onNewMessage(const QByteArray &byteArrayMessage)
+{
+    for(const auto &client: qAsConst(_clients)){
+        client->write(byteArrayMessage);
+        client->flush();
+    }
+
+}
+
+QString tcpServer::getClientKey(const QTcpSocket *client) const
+{
+    return client->peerAddress().toString().append(":").append(QString::number(client->peerPort()));
+}
---
+++ tcpserver.h
@@ -0,0 +1,33 @@
+#ifndef TCPSERVER_H
+#define TCPSERVER_H
+
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QHash>
+
+class tcpServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit tcpServer(QObject *parent = nullptr);
+
+signals:
+    void newMessage(const QByteArray &message);
+
+private slots:
+    void onNewConnection();
+    void onReadyRead();
+    void onDisconnected();
+    void onNewMessage(const QByteArray &byteArrayMessage);
+
+private:
+
+    QTcpServer _server;
+    QHash<QString, QTcpSocket*> _clients;
+
+    QString getClientKey(const QTcpSocket *client) const;
+
+};
+
+#endif // TCPSERVER_H
